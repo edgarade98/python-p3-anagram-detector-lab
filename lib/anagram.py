fe8e4272a0e39c7f85a330b0e3575d2afbab84f8
Edgar Otieno <eddyade98@gmail.com>
2024-01-22 12:29:24 +0300

    Completed Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+    def match (self, list):
+        possible_anagrams = []
+
+        list_word = sorted (letter for letter in self.word)
+        for word in list:
+            if sorted(letter for letter in word) == list_word:
+                possible_anagrams.append(word)
+        return possible_anagrams
+
